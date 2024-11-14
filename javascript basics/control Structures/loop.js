53ec8ecbad1a7bbf23aa2d3f3c1f68905ab09e96
anisioforifunanya <ifunanya.anisiofor@gmail.com>
2024-11-14 13:39:44 +0100

    This class was for loops

--- javascript basics/control Structures/loop.js
+++ javascript basics/control Structures/loop.js
@@ -16,3 +16,11 @@ const fruits=["mango", "orange", "carrot", "pawpaw", "pea"]
 for (let i=0; i<fruits.length; i++) {
     console.log("List of Fruits:", fruits[i]);
 }
+const students = [
+    {name:"john", grade:80},
+    {name:"martins", grade:78},
+    {name:"Seyi", grade:04},
+]
+for(let i=0; i < students.length; i++) {
+    console.log("Student:", students[i].name, "Grade:", students[i].grade);
+}
