b87770e250ceec0fb0ba0d93d6679555631003cd
anisioforifunanya <ifunanya.anisiofor@gmail.com>
2024-11-14 13:09:07 +0100

    This class was for loops

--- javascript basics/control Structures/loop.js
+++ javascript basics/control Structures/loop.js
@@ -1,5 +1,13 @@
 //Loops are used to exectute a block of code repeatedly until specfic conditions are met
 
-for(let i=0; i >5; i++) {
-    console.log("Hello From loop")
+for(let i=0; i <5; i++) {
+    console.log("Hello From loop");
 };
+for(let i=2; i <5; i++) {
+    console.log("Hello From loop " + i );
+};
+
+const colors=["red", "green", "blue", "yellow"]
+for (let i=0; i<colors.length; i++) {
+    console.log("color:", colors[i]);
+}
