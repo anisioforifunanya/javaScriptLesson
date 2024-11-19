a42d03f15d94187b53dab764f759294c6f2b1b0a
anisioforifunanya <ifunanya.anisiofor@gmail.com>
2024-11-19 13:20:50 +0100

    This class was for functionss

---
+++ javascript basics/control Structures/functions.js
@@ -0,0 +1,27 @@
+//Functions: It's a block of code(i.e it's combination of different code) that performs an actions and is re-useable
+
+//Types of Functions
+/*i.)Function Declarations
+ ii.)Function Expression
+ iii.) Arrow function
+*/
+
+//1.) FUnction Declaration for Addition.
+
+function add(a,b) {
+    return(a+b)
+    //return is what the program should process
+}
+
+const sumy = add(2,4);
+console.log(sumy)
+
+//2.) FUnction Declaration for Multiplication.
+
+function producty(a,b) {
+    return(a*b)
+    //return is what the program should process
+}
+
+const multiply = producty(2,4);
+console.log(multiply)
